0027e93c1abd6b3cbe4afc9e9fad331ba16d2a76
andpberr <andrew.berry91@gmail.com>
2020-03-22 14:11:52 -0500

    Updates

---
+++ precache-manifest.ad2f30daa68fa1ebdf6ab6f62ea1211c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5bb5c9c86055e1b6161ff1f2cd9310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c280e858f15ac4035e39",
+    "url": "/static/css/main.8aa1cb6f.chunk.css"
+  },
+  {
+    "revision": "fcae07df941f943166ec",
+    "url": "/static/js/2.ca19c8d3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ca19c8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c280e858f15ac4035e39",
+    "url": "/static/js/main.d0519802.chunk.js"
+  },
+  {
+    "revision": "3948be7fe70aa774e21e",
+    "url": "/static/js/runtime-main.065bbe56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
